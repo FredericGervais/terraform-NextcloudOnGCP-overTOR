a3e8552e5407ebee9f0e53ad225ecd047dc3a997
Frederic <frederic.gervais@outlook.com>
2020-01-03 15:44:22 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -27,3 +27,9 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
+
+resource "google_compute_network_peering" "peering1" {
+  name         = "cloudsql-mysql-googleapis-com"
+  network      = google_compute_network.private_network.self_link
+  peer_network = *********************
+}
