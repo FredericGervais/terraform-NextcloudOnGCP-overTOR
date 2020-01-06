5fff58be90ce8f455ae313501f95a4ecf78c5e83
Frederic <frederic.gervais@outlook.com>
2020-01-06 15:40:47 -0500

    Add files via upload

---
+++ install-base/network.tf
@@ -0,0 +1,38 @@
+#
+# Dependance:
+#
+# Sub-Dependance:
+#   resource "null_resource" "export-custom-routes" : google_sql_database_instance.master
+#
+#
+
+
+resource "google_compute_network" "private_network" {
+  provider = google-beta
+
+  name = var.network-name
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  provider = google-beta
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.private_network.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = google-beta
+
+  network                 = google_compute_network.private_network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "null_resource" "export-custom-routes" {
+  provisioner "local-exec" {
+    command = "gcloud compute networks peerings update cloudsql-mysql-googleapis-com --network ${var.network-name} --export-custom-routes --project ${google_sql_database_instance.master.project}"
+  }
+}
---
+++ install-base/sql-server.tf
@@ -0,0 +1,43 @@
+#
+# Dependance:
+#  google_service_networking_connection.private_vpc_connection
+#  var.region
+#
+
+
+resource "google_sql_database_instance" "master" {
+  provider = google-beta
+
+  name             = "${var.app-name}-database-${random_id.db_name_suffix.hex}"
+  database_version = "MYSQL_5_7"
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-n1-standard-4"
+    availability_type = "REGIONAL"
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.private_network.self_link
+    }
+    backup_configuration {
+      enabled    = true
+      binary_log_enabled = true
+    }
+  }
+}
+
+
+resource "google_sql_database" "database" {
+  name     = var.app-name
+  instance = google_sql_database_instance.master.name
+  charset = "utf8"
+  collation = "utf8_general_ci"
+}
+
+resource "google_sql_user" "users" {
+  name     = "root"
+  instance = google_sql_database_instance.master.name
+  password = random_id.db_user_password.b64_url
+}
