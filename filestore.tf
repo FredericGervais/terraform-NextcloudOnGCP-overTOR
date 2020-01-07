9374df0d79257f6067705f646c6a21775952f860
Frederic <frederic.gervais@outlook.com>
2020-01-07 09:40:04 -0500

    Add files via upload

---
+++ filestore.tf
@@ -0,0 +1,21 @@
+#
+# Dependance:
+#  google_compute_network.private_network
+#
+
+
+resource "google_filestore_instance" "instance" {
+  name = "nfsshare-${random_id.db_name_suffix.hex}"
+  tier = "STANDARD"
+  zone    = "us-east1-b"
+
+  file_shares {
+    capacity_gb = 2048
+    name        = "NFSvol"
+  }
+
+  networks {
+    network = google_compute_network.private_network.name
+    modes   = ["MODE_IPV4"]
+  }
+}
